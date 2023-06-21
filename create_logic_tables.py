ec2b4f10a9b8cf34c648cce30567c99a4925160f
DaveyJH <daveyj.dh@googlemail.com>
2023-06-21 08:01:26 +0100

    feat: Split 4&5-bit adders and add logic

--- create_logic_tables.py
+++ create_logic_tables.py
@@ -18,23 +18,25 @@ def create_table(size):
     There is no error catching...just use it correctly ¯\\\_(ツ)_/¯
     """
 
-    perms = [str(bin(x))[2:].zfill(7) for x in range(size)]
+    perms = [str(bin(x))[2:].zfill(9) for x in range(2 ** (9))]
 
     for x in perms:
         total = 0
         for i, y in enumerate(x):
             space = " " * 6
-            if i in range(6):
+            if i in range(8):
                 space += " "
             if y == "1":
-                if i in [2, 5, 6]:
+                if i in [3, 7, 8]:
                     total += 1
-                if i in [1, 4]:
+                if i in [2, 6]:
                     total += 2
-                if i in [0, 3]:
+                if i in [1, 5]:
                     total += 4
+                if i in [0, 4]:
+                    total += 8
             print(f"|{space}{y}", end="")
-        binary_total = str(bin(total)[2:].zfill(4))
+        binary_total = str(bin(total)[2:].zfill(5))
         for j, z in enumerate(binary_total):
             space = " " * 7
             if j != 0:
